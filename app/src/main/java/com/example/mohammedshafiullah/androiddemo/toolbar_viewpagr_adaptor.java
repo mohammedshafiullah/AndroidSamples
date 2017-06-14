16a19183a894b5aacf3e62f05c242d5f775ade73
mohammedshafiullah <mohammediphone7@gmail.com>
2017-06-14 16:44:15 +0530

    viewPager + toolbar  code Added to the project entaire code

---
+++ app/src/main/java/com/example/mohammedshafiullah/androiddemo/toolbar_viewpagr_adaptor.java
@@ -0,0 +1,19 @@
+package com.example.mohammedshafiullah.androiddemo;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by developer on 14/06/17.
+ */
+
+public class toolbar_viewpagr_adaptor
+{
+
+
+}
+
+
+
