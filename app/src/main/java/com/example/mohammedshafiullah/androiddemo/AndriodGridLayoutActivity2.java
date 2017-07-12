208d3b6e6722a09abb27acd98d09197f2659ab43
mohammedshafiullah <mohammediphone7@gmail.com>
2017-07-12 19:23:00 +0530

    GridView+images is added in the project final project

---
+++ app/src/main/java/com/example/mohammedshafiullah/androiddemo/AndriodGridLayoutActivity2.java
@@ -0,0 +1,31 @@
+package com.example.mohammedshafiullah.androiddemo;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class AndriodGridLayoutActivity2 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_andriod_grid_layout2);
+
+
+        Intent i = getIntent(); //Creating INtetnt here
+        int pos = i.getExtras().getInt("id");//Getting position  from the post Intent
+        AndriodGridLayoutAdaptor imageAdapter = new AndriodGridLayoutAdaptor(this); //calling image Adoptor for images
+
+
+        ViewPager viPgr = (ViewPager) findViewById(R.id.gridview_viewpager);
+        AndriodGridLayoutViewPagerAdaptor viPgrAdp  = new AndriodGridLayoutViewPagerAdaptor (this,imageAdapter.mThumbIds,pos);
+
+        viPgr.setAdapter(viPgrAdp);
+        viPgr.setCurrentItem(pos);
+
+    }
+}
